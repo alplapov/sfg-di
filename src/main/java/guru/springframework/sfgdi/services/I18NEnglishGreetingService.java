ca1d190770cc817a965fe3aae263774a66fece79
Albert <alplapov@gmail.com>
2020-10-04 19:48:33 +0200

    DI with Spring

---
+++ src/main/java/guru/springframework/sfgdi/services/I18NEnglishGreetingService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service("i18NService")
+@Profile("EN")
+public class I18NEnglishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World! --- EN";
+    }
+}
